9b1131e67ecdb203b8a4b5bf7f5a57b2678bb19d
Andrew Theis <andrewtheis4@gmail.com>
2019-06-30 16:38:42 -0700

    Update little_schemer.scm
    Adding additional chapters

--- little_schemer.scm
+++ little_schemer.scm
@@ -71,7 +71,7 @@
   (lambda (l)
     (cond
       ((null? l) '())
-      (else (cons (car (car l)) (firsts (cdr l)))))))
+      (else (cons (caar l) (firsts (cdr l)))))))
 
 ; The Third Commandment
 ; When building a list, describe the first typical element, and then cons it onto the natural recursion.
@@ -141,6 +141,147 @@
 ; ------------
 ; Chapter Four
 ; ------------
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define o-
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (+ (car tup) (addtup (cdr tup)))))))
+
+(define ox
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (+ n (ox n (sub1 m)))))))
+
+; The Fifth Commandment
+; When building a value with +, always use 0 for the value of the terminating line, for adding 0 does not change the value of an addition.
+; When building a value with x, always use 1 for the value of the terminating line, for multiplying by 1 does not change the value of a mutliplication.
+; When building a value with cons, always consider '() for the value of the teriminating line.
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((and (null? tup1) (null? tup2) '()))
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define >
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (< (sub1 n) (sub1 m))))))
+
+(define =
+  (lambda (n m)
+    (cond
+      ((and (zero? n) (zero? m)) #t)
+      ((or (zero? n) (zero? m)) #f)
+      (else (= (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (cond
+      ((> n m) #f)
+      ((< n m) #f)
+      (else #t))))
+
+(define **
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (ox n (** n (sub1 m)))))))
+
+(define ÷
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (÷ (- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+ 
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (o= a1 a2))
+      ((or (number? a1) (number? a2)) #f)
+      (else (eq? a1 a2)))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      ((eqan? a (car lat)) (add1 (occur a (cdr lat))))
+      (else (occur a (cdr lat))))))
+
+(define one?
+  (lambda (n)
+    (o= n 1)))
+
+(define rempick2
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat) (rempick2 (sub1 n) (cdr lat)))))))
 
 
 
